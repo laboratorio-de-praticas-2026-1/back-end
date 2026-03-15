8d2b3bb799920d4ae9992b2a3932648d94d82a72
Pedro-Lucas-Novaes <pedrolucas.novaes.dutra@gmail.com>
2026-03-15 20:36:52 -0300

    chore: fix prettier formatting

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -63,10 +63,10 @@ export class BlogService {
   async deleteById(id: number): Promise<void> {
     const blog = await this.blogModel.findByPk(id);
 
-  if (!blog) {
-    throw new NotFoundException('Post não encontrado');
-  }
+    if (!blog) {
+      throw new NotFoundException('Post não encontrado');
+    }
 
-  await blog.destroy();
-}
+    await blog.destroy();
+  }
 }
