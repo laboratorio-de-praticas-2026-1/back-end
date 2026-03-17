d4d32aa6a9a75a72a8734836dec62e1706ec9e63
Tiago Rodrigues <tr364634@gmail.com>
2026-03-17 14:54:49 -0300

    chore: rebase onto short-release/agendamento

--- src/modules/servicos/servicos.module.ts
+++ src/modules/servicos/servicos.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { ServicosService } from './servicos.service';
 import { ServicosController } from './servicos.controller';
+import { Solicitacao } from '../../models/solicitacao.model';
+import { Usuario } from '../../models/usuario.model';
+import { Veiculo } from '../../models/veiculo.model';
+import { Servico } from '../../models/servico.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Solicitacao, Usuario, Veiculo, Servico])],
   controllers: [ServicosController],
   providers: [ServicosService],
 })
