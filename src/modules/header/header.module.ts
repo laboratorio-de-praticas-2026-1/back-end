8c02ecd08649156479a81207935709f596d66e2a
Erlon Viana dos Santos <erlonviana@hotmail.com>
2026-03-21 13:11:28 -0300

    feat: criação do Endpoint do Carrossel da Página Inicial

--- src/modules/header/header.module.ts
+++ src/modules/header/header.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { HeaderService } from './header.service';
 import { HeaderController } from './header.controller';
+import { Banner } from '../../models/banner.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Banner])],
   controllers: [HeaderController],
   providers: [HeaderService],
 })
--- src/modules/header/header.service.ts
+++ src/modules/header/header.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Banner } from '../../models/banner.model';
 
 @Injectable()
-export class HeaderService {}
+export class HeaderService {
+  constructor(
+    @InjectModel(Banner)
+    private readonly bannerModel: typeof Banner,
+  ) {}
+
+  async getBannersAtivos(): Promise<
+    { id: number; url_imagem: string; descricao: string }[]
+  > {
+    const banners = await this.bannerModel.findAll({
+      where: { ativo: true },
+    });
+
+    return banners.map((banner) => ({
+      id: banner.id,
+      url_imagem: banner.urlImagem || '',
+      descricao: banner.descricao || '',
+    }));
+  }
+}
