a24eca86b3326d15d69c023c97f8d40d77ef7d81
Erlon Viana dos Santos <erlonviana@hotmail.com>
2026-03-21 13:11:28 -0300

    feat: criação do Endpoint do Carrossel da Página Inicial

--- src/modules/header/header.controller.ts
+++ src/modules/header/header.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HeaderService } from './header.service';
 
-@Controller('header')
+@Controller()
 export class HeaderController {
   constructor(private readonly headerService: HeaderService) {}
+
+  @Get('carrossel')
+  async getCarrossel(): Promise<
+    { id: number; url_imagem: string; descricao: string }[]
+  > {
+    return this.headerService.getBannersAtivos();
+  }
 }
