9e5471f0359f3224fdbe9b8fc1ed771bfeeaefd1
TheLimaLeo <leolima1999pro@gmail.com>
2026-03-21 23:50:00 -0300

    fix: ajustando tipagem e estilo para passar no ESLint/Prettier

--- src/core/services/BannerService.ts
+++ src/core/services/BannerService.ts
@@ -2,33 +2,42 @@ import { Banner, CreateBannerInput } from '../entities/Banner';
 
 export class BannerService {
   private static banners: Banner[] = [];
-  private static proximoId = 1; 
+  private static proximoId = 1;
 
   static async listAll(): Promise<Banner[]> {
-    return this.banners;
+    return Promise.resolve(this.banners);
   }
 
   static async create(data: CreateBannerInput): Promise<Banner> {
     const newBanner: Banner = {
-      id: this.proximoId++, 
-      ...data
+      id: this.proximoId++,
+      url_imagem: data.url_imagem,
+      descricao: data.descricao,
+      ativo: data.ativo,
     };
     this.banners.push(newBanner);
-    return newBanner;
+    return Promise.resolve(newBanner);
   }
 
-  static async update(id: number, data: Partial<CreateBannerInput>): Promise<Banner> {
-    const index = this.banners.findIndex(b => b.id === id);
-    if (index === -1) throw new Error("Banner não encontrado");
+  static async update(
+    id: number,
+    data: Partial<CreateBannerInput>,
+  ): Promise<Banner> {
+    const index = this.banners.findIndex((b) => b.id === id);
+    if (index === -1) {
+      throw new Error('Banner não encontrado');
+    }
 
     this.banners[index] = { ...this.banners[index], ...data };
-    return this.banners[index];
+    return Promise.resolve(this.banners[index]);
   }
 
   static async delete(id: number): Promise<void> {
-    const index = this.banners.findIndex(b => b.id === id);
-    if (index === -1) throw new Error("Banner não encontrado");
-    
+    const index = this.banners.findIndex((b) => b.id === id);
+    if (index === -1) {
+      throw new Error('Banner não encontrado');
+    }
     this.banners.splice(index, 1);
+    return Promise.resolve();
   }
-}
+};
