1d776487076895f185b4a3ee6f2ecdb4d995cbfc
Carlos Silva <ChsSilva1419@gmail.com>
2026-03-26 16:10:39 -0300

    feat: adiciona campos tipo, latitude e longitude no modelo empresa

--- src/models/empresa.model.ts
+++ src/models/empresa.model.ts
@@ -33,8 +33,8 @@ export class Empresa extends Model {
   @Column({ type: DataType.STRING(100), allowNull: true })
   declare site: string | null;
 
-  @Column({ type: DataType.ENUM("Clinica","Detran","Parceiras"), allowNull: true })
-  declare tipo: "Clinica" | "Detran" | "Parceiras" | null;
+  @Column({ type: DataType.ENUM('clinica','vistoria','detran'), allowNull: true })
+  declare tipo: 'clinica' | 'vistoria' | 'detran' | null;
 
   @Column({ type: DataType.STRING(100), allowNull: true })
   declare latitude: string | null;
