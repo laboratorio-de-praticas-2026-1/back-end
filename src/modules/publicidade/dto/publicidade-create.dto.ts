af6f6344e7d1aae4e379346e074348d850433bda
Arthur Fudali <arthurfudali5@gmail.com>
2026-03-27 11:15:49 -0300

    docs: adiciona propriedades do swagger

--- src/modules/publicidade/dto/publicidade-create.dto.ts
+++ src/modules/publicidade/dto/publicidade-create.dto.ts
@@ -1,12 +1,36 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PublicidadeCreateDto {
   @IsString()
+  @ApiProperty({
+    example: 'Seguro Auto Completo',
+  })
   titulo: string;
 
   @IsString()
+  @ApiProperty({
+    example: 'Proteja seu veículo com nosso parceiro credenciado.',
+  })
   conteudo: string;
 
   @IsString()
+  @ApiProperty({
+    example: 'https://img.com/pub1.jpg',
+  })
+  urlImagem: string;
+}
+
+export class PublicidadeResponseDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  titulo: string;
+
+  @ApiProperty()
+  conteudo: string;
+
+  @ApiProperty()
   urlImagem: string;
 }
--- src/modules/publicidade/publicidade.controller.ts
+++ src/modules/publicidade/publicidade.controller.ts
@@ -1,15 +1,25 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { PublicidadeService } from './publicidade.service';
-import { PublicidadeCreateDto } from './dto/publicidade-create.dto';
+import {
+  PublicidadeCreateDto,
+  PublicidadeResponseDto,
+} from './dto/publicidade-create.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Publicidade } from '../../models/publicidade.model';
 
+@ApiTags('Publicidade')
 @Controller('publicidade')
 export class PublicidadeController {
   private readonly logger = new Logger(PublicidadeController.name);
 
   constructor(private readonly publicidadeService: PublicidadeService) {}
 
+  @ApiOperation({ summary: 'Criar uma nova publicidade' })
+  @ApiResponse({ status: 201, type: PublicidadeResponseDto })
   @Post()
-  criarPublicidade(@Body() publicidadeDto: PublicidadeCreateDto) {
+  criarPublicidade(
+    @Body() publicidadeDto: PublicidadeCreateDto,
+  ): Promise<Publicidade> {
     this.logger.log(`Iniciando criacao de publicidade...`);
     return this.publicidadeService.criarPublicidade(publicidadeDto);
   }
