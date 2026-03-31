a19c25f38426dadee569a0160075a332983d745d
Sync633 <miguelsantosp633@gmail.com>
2026-03-31 00:30:57 -0300

    feat: criação das funções findAll, findByTipo e findByCidade no mapa.service

--- src/modules/mapa/mapa.module.ts
+++ src/modules/mapa/mapa.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { MapaService } from './mapa.service';
 import { MapaController } from './mapa.controller';
+import { Empresa } from 'src/models/empresa.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Empresa])],
   controllers: [MapaController],
   providers: [MapaService],
 })
--- src/modules/mapa/mapa.service.ts
+++ src/modules/mapa/mapa.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
+import { Empresa } from 'src/models/empresa.model';
+
+const tipos_empresas = ['clinica', 'vistoria', 'detran'];
 
 @Injectable()
-export class MapaService {}
+export class MapaService {
+    constructor(
+        @InjectModel(Empresa)
+        private empresaModel: typeof Empresa
+    ) { }
+
+    private get defaultFiltroMapa() {
+        return {
+            latitude: { [Op.ne]: null },
+            longitude: { [Op.ne]: null },
+        };
+    }
+
+    async findAll(): Promise<Empresa[]> {
+        return this.empresaModel.findAll({
+            where: this.defaultFiltroMapa,
+        });
+    }
+
+    async findByTipo(tipo: string): Promise<Empresa[]> {
+        const tipoFormatado = tipo.toLowerCase();
+
+        if (!tipos_empresas.includes(tipoFormatado)) {
+            throw new BadRequestException(`O tipo '${tipo}' não é válido para o mapa`)
+        }
+
+        return this.empresaModel.findAll({
+            where: {
+                ...this.defaultFiltroMapa,
+                tipo: tipoFormatado,
+            },
+        });
+    }
+
+    async findByCidade(cidade: string): Promise<Empresa[]> {
+        return this.empresaModel.findAll({
+            where: {
+                ...this.defaultFiltroMapa,
+                cidade: {
+                    [Op.like]: `%${cidade}%`,
+                },
+            },
+        });
+    }
+
+}
