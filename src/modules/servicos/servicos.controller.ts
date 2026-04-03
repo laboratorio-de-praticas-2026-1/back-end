fff480d117dfa4c9f86e823c5ae506b6661757f3
DSM3 <fatec-dsm3@academico.intra>
2026-04-02 22:37:44 -0300

    feat:Criação da função createServico

--- src/modules/servicos/servicos.controller.ts
+++ src/modules/servicos/servicos.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { ServicosService } from './servicos.service';
 
 @Controller('servicos')
 export class ServicosController {
   constructor(private readonly servicosService: ServicosService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async criar(@Body() body: { nome: string; descricao?: string }) {
+    await this.servicosService.criarServico(
+      body.nome,
+      body.descricao,
+    );
+
+    return {
+      message: 'Serviço criado com sucesso',
+    };
+  }
 }
--- src/modules/servicos/servicos.service.ts
+++ src/modules/servicos/servicos.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ServicosService {}
+export class ServicosService {
+  constructor(private prisma: PrismaService) {}
+
+  async criarServico(nome: string, descricao?: string) {
+    // validação
+    if (!nome) {
+      throw new BadRequestException('Nome é obrigatório');
+    }
+
+    // criar no banco
+    const servico = await this.prisma.servico.create({
+      data: {
+        nome,
+        descricao,
+      },
+    });
+
+    return servico;
+  }
+}
