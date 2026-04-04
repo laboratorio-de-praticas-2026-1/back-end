55ba1155b2d86b663d300ae8cf3fb452d0aee0b4
Lucas Parada <lucasparada2006@gmail.com>
2026-04-04 05:33:05 -0300

    fix envio email

--- src/modules/contato/contato.module.ts
+++ src/modules/contato/contato.module.ts
@@ -2,22 +2,22 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Empresa } from 'src/models/empresa.model';
-import { ContatoMensagem } from 'src/models/contato-mensagem.model';
+import { EmailEnviado } from 'src/models/email-enviado.model';
 import { ContatoController } from './contato.controller';
 import { ContatoService } from './contato.service';
 import { EmailService } from './email.service';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Empresa, ContatoMensagem]),
+    SequelizeModule.forFeature([Empresa, EmailEnviado]),
     MailerModule.forRoot({
       transport: {
         host: process.env.EMAIL_HOST || 'smtp.gmail.com',
         port: parseInt(process.env.EMAIL_PORT || '587', 10),
         secure: process.env.EMAIL_SECURE === 'true',
         auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASSWORD,
+          user: '',//Email base
+          pass: '', //Chave acesso
         },
       },
       defaults: {
