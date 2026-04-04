50f85345b98b6eb7dfb45eff455f4af587c246c2
Lucas Parada <lucasparada2006@gmail.com>
2026-04-04 05:24:39 -0300

    fix envio email

---
+++ src/models/email-enviado.model.ts
@@ -0,0 +1,44 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table({ tableName: 'emails_enviados', timestamps: false })
+export class EmailEnviado extends Model {
+  
+  @Column({ primaryKey: true, autoIncrement: true, allowNull: false })
+  declare id: number;
+
+  @Column({ 
+    field: 'nome_usuario',
+    type: DataType.STRING(255), 
+    allowNull: false 
+  })
+  declare nomeUsuario: string;
+
+  @Column({ 
+    field: 'email_usuario',
+    type: DataType.STRING(255), 
+    allowNull: false 
+  })
+  declare emailUsuario: string;
+
+  @Column({ 
+    field: 'texto_digitado',
+    type: DataType.TEXT, 
+    allowNull: false 
+  })
+  declare textoDigitado: string;
+
+  @Column({ 
+    field: 'assunto',
+    type: DataType.STRING(255), 
+    allowNull: false 
+  })
+  declare assunto: string;
+
+  @Column({ 
+    field: 'data_envio', 
+    type: DataType.DATE(3), 
+    allowNull: false,
+    defaultValue: DataType.NOW 
+  })
+  declare dataEnvio: Date;
+}
