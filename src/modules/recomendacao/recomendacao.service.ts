e39f89a367d2fa05fe3d2e846477f882bf80222d
MaxPichy <camilamachadodesouza77@gmail.com>
2026-04-05 22:47:06 -0300

    115-feature-implementacao-inicial-da-rota-do-perfil-de-interesse-do-usuario

--- src/modules/recomendacao/recomendacao.service.ts
+++ src/modules/recomendacao/recomendacao.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
-export class RecomendacaoService {}
+export class RecomendacaoService{
+    constructor(
+        @InjectEntityManager()
+        private entityManager: EntityManager,
+    ){}
+    
+    async buscarAtributosPerfil(usuarioId: number){
+        const buscarAtributosPerfil = await this.executarBuscaAtributosPerfil(usuarioId);
+        return buscarAtributosPerfil;
+    }
+
+    private async executarBuscaAtributosPerfil(usuarioId: number){
+        const query =`
+            SELECT 
+                servico.nome,
+                servico.descricao,
+                servico.valor_base,
+                servico.ativo
+            FROM SOLICITACAO solicitacao
+            INNER JOIN SERVICO servico ON solicitacao.servicoId = servico.id
+            WHERE solicitacao.usuarioId = ?
+            AND servico.perfil_recomendacao = true
+        `;
+    
+        const resultado = await this.entityManager.query(query, [usuarioId]);
+        return resultado;
+    }
+}
