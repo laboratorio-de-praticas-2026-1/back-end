7b3e6e8ccde204866a639805e41b636f1767af8c
MaxPichy <camilamachadodesouza77@gmail.com>
2026-04-07 18:36:59 -0300

    estrutura-primaria-recomendacao

--- src/modules/recomendacao/recomendacao.service.ts
+++ src/modules/recomendacao/recomendacao.service.ts
@@ -1,33 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { InjectEntityManager } from '@nestjs/typeorm';
-import { EntityManager } from 'typeorm';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Servico } from 'src/models/servico.model';
+import { Solicitacao } from 'src/models/solicitacao.model';
 
 @Injectable()
-export class RecomendacaoService{
-    constructor(
-        @InjectEntityManager()
-        private entityManager: EntityManager,
-    ){}
-    
-    async buscarAtributosPerfil(usuarioId: number){
-        const buscarAtributosPerfil = await this.executarBuscaAtributosPerfil(usuarioId);
-        return buscarAtributosPerfil;
-    }
+export class RecomendacaoService {
+    private readonly logger = new Logger(RecomendacaoService.name);
 
-    private async executarBuscaAtributosPerfil(usuarioId: number){
-        const query =`
-            SELECT 
-                servico.nome,
-                servico.descricao,
-                servico.valor_base,
-                servico.ativo
-            FROM SOLICITACAO solicitacao
-            INNER JOIN SERVICO servico ON solicitacao.servicoId = servico.id
-            WHERE solicitacao.usuarioId = ?
-            AND servico.perfil_recomendacao = true
-        `;
-    
-        const resultado = await this.entityManager.query(query, [usuarioId]);
-        return resultado;
+    constructor(
+      @InjectModel(Servico)
+      private servicoModel: typeof Servico,
+      @InjectModel(Solicitacao)
+      private solicitacaoModel: typeof Solicitacao
+    ) {}
+    async buscarAtributosPerfil(usuarioId: number): Promise<any[]> {
+        try{
+            this.logger.log(`Buscando serviços do user com id ${usuarioId}`);
+        
+            const atributos = await this.solicitacaoModel.findAll({
+                attributes: [
+                    'servico.nome',
+                    'servico.descricao',
+                    'servico.valor_base',
+                    'servico.ativo'
+                ],
+        
+                where: {
+                    usuarioId: usuarioId,
+                },
+        
+                include: [{
+                    model: this.servicoModel,
+                    attributes: []
+                }],
+                raw: true,
+                nest: true
+                // limit: 5 -> possível implementacao posterior
+            });
+        
+            if(!atributos ||atributos.length == 0){
+                this.logger.warn(`Nenhum serviço com perfil de recomendação encontrado para usuário ${usuarioId}`);
+        
+                throw new NotFoundException(`Nenhum serviço encontrado para o usuário com id ${usuarioId}`);
+            }
+            return atributos;
+
+        } catch(error){
+            this.logger.error(`Erro ao buscar serviços para o user com id ${usuarioId}`);
+        
+            throw new InternalServerErrorException('Erro ao buscar serviços para recomendação');
+        }
     }
 }
