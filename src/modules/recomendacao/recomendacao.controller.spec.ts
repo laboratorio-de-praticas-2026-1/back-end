70cd8de6a9ea705c85197239fd65682d0f07d30c
Giovana Albanês <albanesgiovana@gmail.com>
2026-04-09 15:24:33 -0300

    test: add mocks para o teste

--- src/modules/recomendacao/recomendacao.controller.spec.ts
+++ src/modules/recomendacao/recomendacao.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecomendacaoController } from './recomendacao.controller';
 import { RecomendacaoService } from './recomendacao.service';
+import { getModelToken } from '@nestjs/sequelize';
+import { Servico } from 'src/models/servico.model';
+import { Solicitacao } from 'src/models/solicitacao.model';
+import { describe, it, expect, beforeEach } from '@jest/globals';
 
 describe('RecomendacaoController', () => {
   let controller: RecomendacaoController;
@@ -8,7 +12,17 @@ describe('RecomendacaoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecomendacaoController],
-      providers: [RecomendacaoService],
+      providers: [
+        RecomendacaoService,
+        {
+          provide: getModelToken(Servico),
+          useValue: {},
+        },
+        {
+          provide: getModelToken(Solicitacao),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<RecomendacaoController>(RecomendacaoController);
