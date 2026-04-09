87233be6f65f2dd903343e1161f654fd45bafc9d
Filipe Lima <filipeolv.lima@gmail.com>
2026-04-09 20:41:25 -0300

    docs: adicionar tags swagger no debito.controller

--- src/modules/debito/debito.controller.ts
+++ src/modules/debito/debito.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { DebitoService } from './debito.service';
+import { Controller, Get, Logger, Param } from '@nestjs/common';
+import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { DebitoResponseDto } from './dto/debito-response.dto';
+import { DebitoService } from './debito.service';
 
+@ApiTags('veiculos')
 @Controller('veiculos')
 export class DebitoController {
+  private readonly logger = new Logger(DebitoController.name);
+
   constructor(private readonly debitoService: DebitoService) {}
 
   @Get(':placa/debitos')
+  @ApiOkResponse({ type: DebitoResponseDto })
+  @ApiNotFoundResponse({ description: 'Veículo não encontrado' })
   async buscarDebitos(
     @Param('placa') placa: string,
   ): Promise<DebitoResponseDto> {
+    this.logger.log(`Iniciando busca de débitos para placa: ${placa}`);
     return this.debitoService.buscarDebitosPorPlaca(placa);
   }
 }
