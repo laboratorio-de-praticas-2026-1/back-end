5c2907b8d59f04c2b951d311d0a2c3b1023713b5
Giovana Albanês <albanesgiovana@gmail.com>
2026-04-13 11:58:28 -0300

    refactor: add comentario para esclarecer funcionalidade do DTO

--- src/modules/recomendacao/dto/recomendacao-resposta.dto.ts
+++ src/modules/recomendacao/dto/recomendacao-resposta.dto.ts
@@ -1,27 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+// DTO de resposta da API da rota GET
 export class RecomendacaoRespostaDto {
-  @ApiProperty({ 
-    example: 7, 
-    description: 'ID identificador do serviço' 
+  @ApiProperty({
+    example: 7,
+    description: 'ID identificador do serviço',
   })
   id!: number;
 
-  @ApiProperty({ 
-    example: 'Licenciamento Anual (CRLV-e)', 
-    description: 'Nome do serviço automotivo' 
+  @ApiProperty({
+    example: 'Licenciamento Anual (CRLV-e)',
+    description: 'Nome do serviço automotivo',
   })
   nome!: string;
 
-  @ApiProperty({ 
-    example: 'Processo de renovação do documento do veículo para circulação regular.', 
-    description: 'Descrição detalhada para o usuário' 
+  @ApiProperty({
+    example:
+      'Processo de renovação do documento do veículo para circulação regular.',
+    description: 'Descrição detalhada para o usuário',
   })
   descricao!: string;
 
-  @ApiProperty({ 
-    example: true, 
-    description: 'Status do serviço no catálogo' 
+  @ApiProperty({
+    example: true,
+    description: 'Status do serviço no catálogo',
   })
   ativo!: boolean;
 }
