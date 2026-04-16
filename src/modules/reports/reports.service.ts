5fab4c44cab51901475b78f0ed7f95543f811032
noemifatec <noemi.henrique@aluno.cps.sp.gov.br>
2026-04-16 15:55:31 -0300

    Endpoints para categorias de reports.

--- src/modules/reports/reports.service.ts
+++ src/modules/reports/reports.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+
+  getCategorias() {
+    return {
+      categorias: [
+        {
+          nome: "Relatório Completo",
+          endpoint: "/reports/categorias/completo"
+        },
+        {
+          nome: "Performance Financeira",
+          endpoint: "/reports/categorias/financeiro"
+        },
+        {
+          nome: "Desempenho Operacional",
+          endpoint: "/reports/categorias/operacional"
+        }
+      ]
+    };
+  }
+
+  getRelatorioCompleto() {
+    return { categoria: "Relatório Completo" };
+  }
+
+  getPerformanceFinanceira() {
+    return { categoria: "Performance Financeira" };
+  }
+
+  getDesempenhoOperacional() {
+    return { categoria: "Desempenho Operacional" };
+  }
+
+  getPerformanceServicos() {
+    return { categoria: "Performance de Serviços" };
+  }
+
+  getGestaoSolicitacoes() {
+    return { categoria: "Gestão de Solicitações" };
+  }
+
+  getGestaoDocumentos() {
+    return { categoria: "Gestão de Documentos" };
+  }
+
+  getGestaoVeiculos() {
+    return { categoria: "Gestão de Veículos" };
+  }
+
+  getBaseClientes() {
+    return { categoria: "Base de Clientes" };
+  }
+
+  getAnaliseEficiencia() {
+    return { categoria: "Análise de Eficiência" };
+  }
+
+  getFunilConversao() {
+    return { categoria: "Funil de Conversão" };
+  }
+
+  getGargalosOperacionais() {
+    return { categoria: "Gargalos Operacionais" };
+  }
+}
