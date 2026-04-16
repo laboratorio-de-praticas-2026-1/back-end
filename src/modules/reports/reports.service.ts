a94b39340009c1b460864eb13f2c31b19324f958
noemifatec <noemi.henrique@aluno.cps.sp.gov.br>
2026-04-16 16:50:23 -0300

    Update reports.service.ts Trocando aspas duplas por aspas simples.

--- src/modules/reports/reports.service.ts
+++ src/modules/reports/reports.service.ts
@@ -2,67 +2,100 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ReportsService {
-
   getCategorias() {
     return {
       categorias: [
         {
-          nome: "Relatório Completo",
-          endpoint: "/reports/categorias/completo"
+          nome: 'Relatório Completo',
+          endpoint: '/reports/categorias/completo',
         },
         {
-          nome: "Performance Financeira",
-          endpoint: "/reports/categorias/financeiro"
+          nome: 'Performance Financeira',
+          endpoint: '/reports/categorias/financeiro',
         },
         {
-          nome: "Desempenho Operacional",
-          endpoint: "/reports/categorias/operacional"
-        }
-      ]
+          nome: 'Desempenho Operacional',
+          endpoint: '/reports/categorias/operacional',
+          subcategorias: [
+            {
+              nome: 'Performance de Serviços',
+              endpoint: '/reports/categorias/operacional/servicos',
+            },
+            {
+              nome: 'Gestão de Solicitações',
+              endpoint: '/reports/categorias/operacional/solicitacoes',
+            },
+            {
+              nome: 'Gestão de Documentos',
+              endpoint: '/reports/categorias/operacional/documentos',
+            },
+            {
+              nome: 'Gestão de Veículos',
+              endpoint: '/reports/categorias/operacional/veiculos',
+            },
+            {
+              nome: 'Base de Clientes',
+              endpoint: '/reports/categorias/operacional/clientes',
+            },
+            {
+              nome: 'Análise de Eficiência',
+              endpoint: '/reports/categorias/operacional/eficiencia',
+            },
+            {
+              nome: 'Funil de Conversão',
+              endpoint: '/reports/categorias/operacional/funil',
+            },
+            {
+              nome: 'Gargalos Operacionais',
+              endpoint: '/reports/categorias/operacional/gargalos',
+            },
+          ],
+        },
+      ],
     };
   }
 
   getRelatorioCompleto() {
-    return { categoria: "Relatório Completo" };
+    return { categoria: 'Relatório Completo' };
   }
 
   getPerformanceFinanceira() {
-    return { categoria: "Performance Financeira" };
+    return { categoria: 'Performance Financeira' };
   }
 
   getDesempenhoOperacional() {
-    return { categoria: "Desempenho Operacional" };
+    return { categoria: 'Desempenho Operacional' };
   }
 
   getPerformanceServicos() {
-    return { categoria: "Performance de Serviços" };
+    return { categoria: 'Performance de Serviços' };
   }
 
   getGestaoSolicitacoes() {
-    return { categoria: "Gestão de Solicitações" };
+    return { categoria: 'Gestão de Solicitações' };
   }
 
   getGestaoDocumentos() {
-    return { categoria: "Gestão de Documentos" };
+    return { categoria: 'Gestão de Documentos' };
   }
 
   getGestaoVeiculos() {
-    return { categoria: "Gestão de Veículos" };
+    return { categoria: 'Gestão de Veículos' };
   }
 
   getBaseClientes() {
-    return { categoria: "Base de Clientes" };
+    return { categoria: 'Base de Clientes' };
   }
 
   getAnaliseEficiencia() {
-    return { categoria: "Análise de Eficiência" };
+    return { categoria: 'Análise de Eficiência' };
   }
 
   getFunilConversao() {
-    return { categoria: "Funil de Conversão" };
+    return { categoria: 'Funil de Conversão' };
   }
 
   getGargalosOperacionais() {
-    return { categoria: "Gargalos Operacionais" };
+    return { categoria: 'Gargalos Operacionais' };
   }
 }
