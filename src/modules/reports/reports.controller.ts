08d5ffabace3deaa6b70ff12a70d1eb808c9c3ba
noemifatec <noemi.henrique@aluno.cps.sp.gov.br>
2026-04-16 15:47:52 -0300

    Update reports.controller.ts

--- src/modules/reports/reports.controller.ts
+++ src/modules/reports/reports.controller.ts
@@ -1,7 +1,68 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 
-@Controller('reports')
+@Controller('reports/categorias')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
+
+  // Página principal
+  @Get()
+  getCategorias() {
+    return this.reportsService.getCategorias();
+  }
+
+  @Get('completo')
+  getRelatorioCompleto() {
+    return this.reportsService.getRelatorioCompleto();
+  }
+
+  @Get('financeiro')
+  getPerformanceFinanceira() {
+    return this.reportsService.getPerformanceFinanceira();
+  }
+
+  @Get('operacional')
+  getDesempenhoOperacional() {
+    return this.reportsService.getDesempenhoOperacional();
+  }
+
+  @Get('operacional/servicos')
+  getPerformanceServicos() {
+    return this.reportsService.getPerformanceServicos();
+  }
+
+  @Get('operacional/solicitacoes')
+  getGestaoSolicitacoes() {
+    return this.reportsService.getGestaoSolicitacoes();
+  }
+
+  @Get('operacional/documentos')
+  getGestaoDocumentos() {
+    return this.reportsService.getGestaoDocumentos();
+  }
+
+  @Get('operacional/veiculos')
+  getGestaoVeiculos() {
+    return this.reportsService.getGestaoVeiculos();
+  }
+
+  @Get('operacional/clientes')
+  getBaseClientes() {
+    return this.reportsService.getBaseClientes();
+  }
+
+  @Get('operacional/eficiencia')
+  getAnaliseEficiencia() {
+    return this.reportsService.getAnaliseEficiencia();
+  }
+
+  @Get('operacional/funil')
+  getFunilConversao() {
+    return this.reportsService.getFunilConversao();
+  }
+
+  @Get('operacional/gargalos')
+  getGargalosOperacionais() {
+    return this.reportsService.getGargalosOperacionais();
+  }
 }
