1ab1d774c0da84634286749fa64497f52520a53d
noemifatec <noemi.henrique@aluno.cps.sp.gov.br>
2026-04-17 12:34:09 -0300

    Update reports.service.ts

--- src/modules/reports/reports.service.ts
+++ src/modules/reports/reports.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  getCategorias() {
+    return [
+      { nome: 'Relatório Completo', valor: 'completo' },
+      { nome: 'Performance Financeira', valor: 'financeiro' },
+      { nome: 'Desempenho Operacional', valor: 'operacional' },
+      { nome: 'Performance de Serviços', valor: 'servicos' },
+      { nome: 'Gestão de Solicitações', valor: 'solicitacoes' },
+      { nome: 'Gestão de Documentos', valor: 'documentos' },
+      { nome: 'Gestão de Veículos', valor: 'veiculos' },
+      { nome: 'Base de Clientes', valor: 'clientes' },
+      { nome: 'Análise de Eficiência', valor: 'eficiencia' },
+      { nome: 'Funil de Conversão', valor: 'funil' },
+      { nome: 'Gargalos Operacionais', valor: 'gargalos' }
+    ];
+  }
+}
