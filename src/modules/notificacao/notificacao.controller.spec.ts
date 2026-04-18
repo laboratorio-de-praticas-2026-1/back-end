b19f33174a181abb212b31e8d4d5f0615d702f4e
Richardo-o <rickysilva04205@gmail.com>
2026-04-18 16:00:35 -0300

    fix: remove external entity imports from tests (use local mocks)

--- src/modules/notificacao/notificacao.controller.spec.ts
+++ src/modules/notificacao/notificacao.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificacaoController } from './notificacao.controller';
 import { NotificacaoService } from './notificacao.service';
+import { getModelToken } from '@nestjs/sequelize';
+
+const mockSequelize = {};
+const mockDebitoModel = {};
 
 describe('NotificacaoController', () => {
   let controller: NotificacaoController;
@@ -8,7 +12,11 @@ describe('NotificacaoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificacaoController],
-      providers: [NotificacaoService],
+      providers: [
+        NotificacaoService,
+        { provide: 'SEQUELIZE', useValue: mockSequelize },
+        { provide: getModelToken('Debito'), useValue: mockDebitoModel },
+      ],
     }).compile();
 
     controller = module.get<NotificacaoController>(NotificacaoController);
