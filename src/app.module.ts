235f70ef57f62d5afcb56bdbf05964ea85314e4a
Isaac Hideki <hidekisaac1@gmail.com>
2026-04-29 19:26:46 -0300

    fix: alteração na importacao main

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,10 +15,10 @@ import { FaqModule } from './modules/faq/faq.module';
 import { HeaderModule } from './modules/header/header.module';
 import { MapaModule } from './modules/mapa/mapa.module';
 import { NotificacaoModule } from './modules/notificacao/notificacao.module';
-import { PagamentoModule } from './modules/pagamento/pagamento.module';
 import { PublicidadeModule } from './modules/publicidade/publicidade.module';
 import { RecomendacaoModule } from './modules/recomendacao/recomendacao.module';
 import { ReportsModule } from './modules/reports/reports.module';
+import { PagamentoModule } from './modules/pagamento/pagamento.module';
 import { ServicosModule } from './modules/servicos/servicos.module';
 import { SimuladorModule } from './modules/simulador/simulador.module';
 import { SolicitacaoModule } from './modules/solicitacao/solicitacao.module';
@@ -53,9 +53,9 @@ import { UsuarioModule } from './modules/usuario/usuario.module';
     HeaderModule,
     BuscaModule,
     ServicosModule,
+    PagamentoModule,
     PublicidadeModule,
     EmailModule,
-    PagamentoModule,
     CloudinaryModule,
     SolicitacaoModule,
     UtilsModule,
