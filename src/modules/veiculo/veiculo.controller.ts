07616330b2cd20b3fd64e14c99d707d9484c3799
José Vitor <sinkktzez@gmail.com>
2026-05-01 16:50:32 -0300

    fix: ajusta getAll e getById para Swagger

--- src/modules/veiculo/veiculo.controller.ts
+++ src/modules/veiculo/veiculo.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Get, Logger, Param, ParseIntPipe } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiParam,
+} from '@nestjs/swagger';
 import { Veiculo } from 'src/models/veiculo.model';
 import { VeiculoService } from './veiculo.service';
 
+@ApiTags('Veículo')
 @Controller('veiculo')
 export class VeiculoController {
   private readonly logger = new Logger(VeiculoController.name);
@@ -10,14 +16,18 @@ export class VeiculoController {
   constructor(private readonly veiculoService: VeiculoService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Busca todos os veículos' })
+  @ApiOperation({ summary: 'Listar todos os veículos' })
+  @ApiResponse({ status: 200, type: Veiculo, isArray: true })
   async getAll(): Promise<Veiculo[]> {
     this.logger.log('Iniciando busca de todos os veículos...');
     return this.veiculoService.getAll();
   }
 
   @Get(':id')
-  @ApiOperation({ summary: 'Busca um veículo pelo ID' })
+  @ApiOperation({ summary: 'Buscar um veículo pelo ID' })
+  @ApiParam({ name: 'id', type: 'number', description: 'ID do veículo' })
+  @ApiResponse({ status: 200, type: Veiculo })
+  @ApiResponse({ status: 404, description: 'Veículo não encontrado' })
   async getById(@Param('id', ParseIntPipe) id: number): Promise<Veiculo> {
     this.logger.log(`Iniciando busca de veículo por ID: ${id}`);
     return this.veiculoService.getById(id);
