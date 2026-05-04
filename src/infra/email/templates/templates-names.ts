7b1e7e605903837ee8c0853ffc850bfe39db27aa
Arthur Fudali <arthur.fudali@gmail.com>
2026-05-03 22:56:05 -0300

    fix: corrige importacao de template

--- src/infra/email/templates/templates-names.ts
+++ src/infra/email/templates/templates-names.ts
@@ -2,3 +2,4 @@ export const CONTATO_DUVIDA_CLIENTE = 'contato';
 export const NOTIFICACAO_DEBITOS_PRAZOS = 'notificacao-debitos-prazos';
 export const STATUS_UPDATE = 'status-update';
 export const SOLICITACAO_FEITA = 'solicitacao-feita';
+export const NOTIFICACAO_DEBITOS_PRAZOS = 'notificacao-debitos-prazos';
